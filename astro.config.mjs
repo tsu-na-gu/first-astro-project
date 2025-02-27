7f90929199d3af88d537adfec9a623d180d2858c
tsu-na-gu <skybluecyclist@gmail.com>
2025-02-27 10:35:35 +0900

    top page created

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,21 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {    
-    plugins: [tailwindcss()],  
+    plugins: [tailwindcss({
+      applyBaseStyles: false,
+    })],  
   },
+
   output: 'server',
+
   adapter: vercel({
     edgeMiddleware: true,
   }),
+
+  integrations: [react()],
 });
