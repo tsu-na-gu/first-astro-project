4be61ecc636c414b7d5000bb838fab8b9defea6f
tsu-na-gu <skybluecyclist@gmail.com>
2025-02-27 12:01:59 +0900

    server configuration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
 
-import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {    
+  vite: {
     plugins: [tailwindcss({
-      applyBaseStyles: false,
-    })],  
+      applyBaseStyles: false
+    })]
   },
-
   output: 'server',
-
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
-
-  integrations: [react()],
+  adapter: vercel()
 });
